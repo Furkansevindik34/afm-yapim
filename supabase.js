2c8cb6b638f7df6228f6abb196b9db95b97790e9
AFM YAPIM <pabloescobar8615@gmail.com>
2026-09-20 00:36:59 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://rdzfztcpyephimlcsyfs.supabase.co;
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkemZ6dGNweWVwaGltbGNzeWZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk4NDE1NjAsImV4cCI6MjEwNTQxNzU2MH0.QQIA9Dmq60WdEZpwusGWVJFzmKUkFk4DE2Em7Bbe1as;
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
